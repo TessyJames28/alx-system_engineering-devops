c3382ec076197de37e6a8c41784756ef0db0a445
TessyJames28 <tessyjames28@gmail.com>
2023-03-24 14:40:55 +0100

    creating a file with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this manifest creates a file in /tmp and gives permission
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
