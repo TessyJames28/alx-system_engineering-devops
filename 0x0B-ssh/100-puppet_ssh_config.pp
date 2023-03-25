d2ddc8ecd7f31c075232a725aa46e3f55c95e222
TessyJames28 <tessyjames28@gmail.com>
2023-03-25 18:43:29 +0100

    puppet manifest for ssh configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# manifest that configure client ssh to connect without password
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
