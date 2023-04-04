223535c850ca2fbd4ceda21eb9394bbb38d49558
TessyJames28 <tessyjames28@gmail.com>
2023-04-04 01:12:13 +0100

    manifest that configure custom server header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,22 @@
 #manifest that setup a custom HTTP header for web server
 
-package {"haproxy":
-ensure   => "installed",
-provider => "apt"
+package {'install nginx':
+  ensure   => 'installed',
+  provider => 'apt',
 }
 
-exec {"nginx header":
-command  => '/usr/sbin/ufw allow "Nginx HTTP"'
+exec {'allow http':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"'
 }
 
-file_line {"custom header":
-ensure   => "present",
-path     => "/etc/nginx
-after    => "sendfile on",
-line     => "	add_header X-Served-By $(hostname);"
+file_line {'custom header':
+  ensure => 'present',
+  path   => '/etc/nginx/nginx.conf',
+  after  => 'http {',
+  line   => "	add_header X-Served-By ${hostname};",
 }
 
-service {"nginx":
-ensure   => "running",
-enable   => true
+service {'nginx':
+  ensure => running,
+  enable => true
 }
