0b51eb7e92c45f33a5cd0c503f0818846b9e51b3
TessyJames28 <tessyjames28@gmail.com>
2023-04-24 20:51:54 +0100

    export result to a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that uses REST API to fetch his/her todo list based on ID
+export to a csv file
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "users/{}/todos".format(user_id)).json()
+
+    total_task = 0
+    task_completed = 0
+    list_completed = []
+
+    name = user.get("username")
+
+    with open("{}.csv".format(user_id), "w", newline="") as file:
+        for task in todo:
+            title = task.get("title")
+            task_status = task.get("completed")
+            write = '"{}","{}","{}","{}"\n'
+            file.write(write.format(user_id, name, task_status, title))
