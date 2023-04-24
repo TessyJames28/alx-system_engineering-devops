05dd79b997bd1c96faa3dcb1a3dd200ec9a912e4
TessyJames28 <tessyjames28@gmail.com>
2023-04-24 18:18:32 +0100

    Gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that uses REST API to fetch his/her todo list based on ID"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(id)).json
+    todo = requests.get(url + "users/{}/todos".format(id)).json
+
+    total_task = 0
+    task_completed = 0
+    list_completed = []
+
+    for task in todo:
+        total_task += 1
+        if task.get("completed") is True:
+            task_completed += 1
+            list_completed.append(task.get("title"))
+
+    statement = "Employee {} is done with tasks({}/{}):"
+    print(statement.format(user.get("name"), task_completed, total_task))
+    for tasks in list_completed:
+        print("\t {}".format(tasks))
