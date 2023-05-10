281958d6576f817d44a728ba20211b5dfa2a47b3
TessyJames28 <tessyjames28@gmail.com>
2023-05-10 14:08:06 +0100

    puppet file that resolve 500 server error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # puppet script that fix 500 server error for  a GET method
 
-file { 'replace':
-  ensure   => file,
+exec { 'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
